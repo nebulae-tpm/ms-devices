dabb22bc69940350d11f3d988342254fb19d6a2a
Camilo Zapata <ccamilozt@gmail.com>
2018-05-16 14:32:15 -0500

    se agrega entrypoint.sh para controlar el inicio de los PODs del despliegue

---
+++ backend/devices/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -x
+
+if [ -d /opt/flags ]; then
+	sync -f /opt/flags
+	if [ ! -f /opt/flags/lock_initialize ]; then
+		sleep $(( $RANDOM % 10 ))
+		echo $(hostname) > /opt/flags/lock_initialize
+		sync /opt/flags/lock_initialize
+		sleep $(( $RANDOM % 10 ))
+		if [ "$(hostname)" = "$(cat /opt/flags/lock_initialize)" ]; then
+
+			##########################################################################
+			echo "ESTA LINEA SE REEMPLAZA POR EL COMANDO npm PARA LA INITICIALIZACION"
+			sleep $(( $RANDOM % 60 ))
+			##########################################################################
+
+			echo $(hostname) > /opt/flags/lock_finalize
+			sync /opt/flags/lock_finalize
+		fi
+	fi
+	while [ ! -f /opt/flags/lock_finalize ]; do
+		sleep $(( $RANDOM % 20 ))
+	done
+fi
+
+exec "$@"
