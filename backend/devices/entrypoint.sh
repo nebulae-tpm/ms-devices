dee3e6335d6162443cba1f5f87146af4ab350e40
Camilo Zapata <ccamilozt@gmail.com>
2018-05-17 16:54:51 -0500

    se agrega binario de lock/wait para el inicio del pod del micorbackend

--- backend/devices/entrypoint.sh
+++ backend/devices/entrypoint.sh
@@ -7,7 +7,7 @@ sleep $(( $RANDOM % 10 ))
 while [ 0 ]; do
 	# exec lock
 	/mbinit --step 1 --podName "$(hostname)" --url "$MONGODB_URL" \
-		&& echo "ESTA LINEA SE REEMPLAZA POR EL COMANDO npm PARA LA INITICIALIZACION" \
+		&& echo "ESTA LINEA SE REEMPLAZA POR EL COMANDO npm PARA LA INITICIALIZACION"
            	#############################################################################
 
 	# exec wait
