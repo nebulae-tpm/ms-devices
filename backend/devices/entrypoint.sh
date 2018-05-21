570b72001afe715a3cc4fff0da53a7af543000c1
Camilo Zapata <ccamilozt@gmail.com>
2018-05-21 15:38:12 -0500

    add buildVersion param to mbinit binary

--- backend/devices/entrypoint.sh
+++ backend/devices/entrypoint.sh
@@ -6,14 +6,14 @@ sleep $(( $RANDOM % 10 ))
 
 while [ 0 ]; do
 	# exec lock
-	/mbinit --step 1 --podName "$(hostname)" --url "$MONGODB_URL" \
+	/mbinit --step 1 --podName "$(hostname)" --buildVersion "$DOCKER_VERSION" --url "$MONGODB_URL" \
 		&& echo "ESTA LINEA SE REEMPLAZA POR EL COMANDO npm PARA LA INITICIALIZACION"
            	#############################################################################
 
 	# exec wait
-	/mbinit --step 2 --podName "$(hostname)" --url "$MONGODB_URL" || continue
+	/mbinit --step 2 --podName "$(hostname)" --buildVersion "$DOCKER_VERSION" --url "$MONGODB_URL" || continue
 	# exec unlock
-	/mbinit --step 3 --podName "$(hostname)" --url "$MONGODB_URL" || continue
+	/mbinit --step 3 --podName "$(hostname)" --buildVersion "$DOCKER_VERSION" --url "$MONGODB_URL" || continue
 
 	break
 done
