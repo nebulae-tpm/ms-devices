7c734a3dd04b1592bca91b22cd3e3ad748514c93
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-10-09 11:38:59 -0500

    Adds option --memory to docker build and removes a log in backend

--- backend/devices/bin/services/event-store/EventStoreService.js
+++ backend/devices/bin/services/event-store/EventStoreService.js
@@ -76,7 +76,9 @@ class EventStoreService {
           )
         )
         .subscribe(
-          evt => console.log(`EventStoreService: ${eventType} process: ${evt}`),
+          evt => {
+            //console.log(`EventStoreService: ${eventType} process: ${evt}`)
+          },
           onErrorHandler,
           onCompleteHandler
         );
